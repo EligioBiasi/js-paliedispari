b30ebaa2c496b911e727bae36fad2020c2069d4c
EligioBiasi <biasi.eligio@edufaimm.it>
2023-05-22 17:12:07 +0200

    set up odd-even game

--- script.js
+++ script.js
@@ -19,3 +19,22 @@ function isPalindrome(word){
     }
         return true
 }
+
+
+
+
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+const userNumber = parseInt(document.getElementById('user-number').value);
+
+document.getElementById('odd-button').addEventListener('click', function(){
+    
+})
+
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+function randomNumber(pcnumber){
+    Math.floor(Math.random() * 5) + 1;
+}
+// Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+
+// Dichiariamo chi ha vinto.
