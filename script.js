6cf0937564971e338de738fe39a16ad94ab5fe01
EligioBiasi <biasi.eligio@edufaimm.it>
2023-05-22 16:22:26 +0200

    fixed function

--- script.js
+++ script.js
@@ -2,23 +2,21 @@
 
 // Chiedere all’utente di inserire una parola:
 const insertWord = document.getElementById('user-word').value;
-let checker = true
 document.getElementById('button-word').addEventListener('click', function(){
+    console.log(insertWord)
     if(isPalindrome(insertWord)){
         document.getElementById('output-word').innerHTML='la tua parola è palindroma'
     }else{
         document.getElementById('output-word').innerHTML='la tua parola non è palindroma'
-
     }
 })
 
 // Creare una funzione per capire se la parola inserita è palindroma
 function isPalindrome(word){
-    for(let i=0;i<insertWord.length/2;i++){
-        if(insertWord[i]!==insertWord[insertWord.length-i-1]){
-            checker = true
-        } else{
-            checker = false
-        }
+    for(let i=0;i<word.length/2;i++){
+        if(word[i]!==word[word.length-i-1]){
+            return false
+        } 
     }
+        return true
 }
